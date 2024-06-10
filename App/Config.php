5d45bca302d9a49e3cc49bf1475e3a094bac1f84
valzza <valzadalipi2@gmail.com>
2024-06-10 13:03:38 +0200

    Models/Config
    kirjimi i file config.php

---
+++ App/Config.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_DRIVER = '';
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = '';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = '';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = '';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+}
