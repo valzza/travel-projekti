42b9213dbbd1d2c07a679cb943ba4cfc4bbdec38
valzza <valzadalipi2@gmail.com>
2024-06-11 15:56:18 +0200

    Created Controllers

---
+++ App/Controllers/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Category;
+use \Core\View;
+use \Core\Controller;
+
+/**
+ * Home controller
+ */
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        $categories = Category::orderBy('id', 'desc')->get();
+
+        View::renderTemplate('Categories/index.html', ['categories' => $categories]);
+    }
+
+    public function create()
+    {
+        View::renderTemplate('Categories/create.html');
+    }
+
+    public function store()
+    {
+        $category = new Category();
+        $category->name = $_POST['name'];
+        $category->save();
+        header("Location: /categories");
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+        $id = $_GET['id'];
+        $category = Category::findOrFail($id);
+
+        View::renderTemplate('Categories/edit.html',['category' => $category]);
+    }
+
+    public function update()
+    {
+        $id = $_POST['id'];
+        $category = Category::findOrFail($id);
+        $category->name = $_POST['name'];
+        $category->save();
+        header("Location: /categories");
+    }
+
+    public function destroy()
+    {
+        $id = $_GET['id'];
+        $category = Category::findOrFail($id);
+        $category->delete();
+        header("Location: /categories");
+    }
+
+}
---
+++ App/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Category;
+use \Core\View;
+use \Core\Controller;
+
+/**
+ * Home controller
+ */
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->get();
+
+        View::renderTemplate('Users/index.html', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        View::renderTemplate('Users/create.html');
+    }
+
+    public function store()
+    {
+        $user = new User();
+        $user->name = $_POST['name'];
+        $user->save();
+        header("Location: /users");
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+        $id = $_GET['id'];
+        $user = User::findOrFail($id);
+
+        View::renderTemplate('User/edit.html',['user' => $user]);
+    }
+
+    public function update()
+    {
+        $id = $_POST['id'];
+        $user = User::findOrFail($id);
+        $user->name = $_POST['name'];
+        $user->save();
+        header("Location: /users");
+    }
+
+    public function destroy()
+    {
+        $id = $_GET['id'];
+        $user = User::findOrFail($id);
+        $user->delete();
+        header("Location: /users");
+    }
+
+}
