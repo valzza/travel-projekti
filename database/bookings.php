cfa57c48071bc3bc846e09b797981900c25c5829
valzza <valzadalipi2@gmail.com>
2024-06-11 14:42:05 +0200

    Update bookings.php

--- database/bookings.php
+++ database/bookings.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 Capsule::schema()->create('bookings', function ($table) {
     $table->id();
+    $table->integer('client_id');
     $table->string('where_to', 30);
     $table->string('how_many', 30);
-    $table->date('check_in');
-    $table->date('check_out');
+    $table->datetime('check_in');
+    $table->datetime('check_out');
     $table->string('details', 250);
     $table->timestamps();
 });
